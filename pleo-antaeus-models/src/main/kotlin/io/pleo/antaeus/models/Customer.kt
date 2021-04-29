ad4e5447675c7b8707165397cbbff2ceb99d3cec
Ibiyemi Adewakun <ibywaks@gmail.com>
2021-04-29 14:13:52 +0100

    create new model - subscription and update models - customer, invoice for subscription

--- pleo-antaeus-models/src/main/kotlin/io/pleo/antaeus/models/Customer.kt
+++ pleo-antaeus-models/src/main/kotlin/io/pleo/antaeus/models/Customer.kt
@@ -2,5 +2,9 @@ package io.pleo.antaeus.models
 
 data class Customer(
     val id: Int,
-    val currency: Currency
+    val currency: Currency,
+    val status: CustomerStatus,
+    val createdAt: Long,
+    val updatedAt: Long,
+    val deletedAt: Long
 )
--- pleo-antaeus-models/src/main/kotlin/io/pleo/antaeus/models/Subscription.kt
+++ pleo-antaeus-models/src/main/kotlin/io/pleo/antaeus/models/Subscription.kt
@@ -1,12 +1,9 @@
 package io.pleo.antaeus.models
 
-data class Invoice(
+data class Subscription(
     val id: Int,
     val customerId: Int,
-    val subscriptionId: Int,
     val amount: Money,
-    val status: InvoiceStatus,
-    val paymentRef: String,
     val createdAt: Long,
     val updatedAt: Long,
     val deletedAt: Long
