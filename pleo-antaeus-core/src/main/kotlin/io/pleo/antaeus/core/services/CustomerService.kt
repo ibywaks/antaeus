517d96babd540a62cebdfc5e38fd20ebd37cd6c4
Ibiyemi Adewakun <ibywaks@gmail.com>
2021-05-01 18:23:16 +0100

    updated customer service methods for update and create

--- pleo-antaeus-core/src/main/kotlin/io/pleo/antaeus/core/services/CustomerService.kt
+++ pleo-antaeus-core/src/main/kotlin/io/pleo/antaeus/core/services/CustomerService.kt
@@ -4,10 +4,13 @@
 
 package io.pleo.antaeus.core.services
 
+import io.pleo.antaeus.core.exceptions.CustomerNotCreatedException
 import io.pleo.antaeus.core.exceptions.CustomerNotFoundException
 import io.pleo.antaeus.data.AntaeusDal
+import io.pleo.antaeus.models.Currency
 import io.pleo.antaeus.models.Customer
 import io.pleo.antaeus.models.CustomerStatus
+import io.pleo.antaeus.models.CustomerUpdateSchema
 
 class CustomerService(private val dal: AntaeusDal) {
     fun fetchAll(isDeleted: Boolean = false, status: CustomerStatus? = null): List<Customer> {
@@ -16,5 +19,13 @@ class CustomerService(private val dal: AntaeusDal) {
 
     fun fetch(id: Int): Customer {
         return dal.fetchCustomer(id) ?: throw CustomerNotFoundException(id)
+    }
+
+    fun update(id: Int, updates: CustomerUpdateSchema): Customer {
+        return dal.updateCustomer(id, updates) ?: throw CustomerNotFoundException(id)
+    }
+
+    fun create(currency: Currency): Customer {
+        return dal.createCustomer(currency) ?: throw CustomerNotCreatedException()
     }
 }
